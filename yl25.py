5165e6dd9356f85092ec7a8c127b22c00132a1d3
JakobVapper <jakob.vapper@ametikool.ee>
2023-01-19 11:42:18 +0200

    updates

--- yl25.py
+++ yl25.py
@@ -13,3 +13,20 @@ me['magustoit'] = 'jäätis'
 for k, v in me.items():
     print(k, v)
 
+#me['personal_code'] = '12345678910'
+
+if 'personal_code' in me:
+    print(me['There is a personal code in the dictionary'])
+else:
+    print('There is no personal code in the dictionary')
+
+print(len(me))
+
+me = {
+  'eesnimi': 'Jakob',
+  'perenimi': 'Vapper',
+  'sünniaasta': 2006,
+  'elukoht': 'Muhu',
+  'magustoit': 'kohuke'
+  'pikkus': 1.8,
+}

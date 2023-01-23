96de09e8309ddc8a15c21c6127d2b25038b8fca7
JakobVapper <jakob.vapper@ametikool.ee>
2023-01-23 09:00:11 +0200

    update

--- yl26.py
+++ yl26.py
@@ -1,26 +1,33 @@
-def sales(data):
-    lines = [line.strip().split() for line in data.split("\n") if line]
+def parse_table(name, lines):
+    results = {}
+    stop_word = ("Revenue" if name == "Expenses" else "Expenses")
+    index = lines.index(name) + 1
+    column_names = lines[index].split()
+    index += 1
+    rows = {}
+    while index < len(lines) and lines[index] != stop_word:
+            r = lines[index].split()
+            rows[r[0]] = [int(e) for e in r[1:]]
+            index+=1
+    for col_idx, column_name in enumerate(column_names):
+        results[column_name] = {}
+        for row_index in rows:
+            results[column_name][row_index] = rows[row_index][col_idx]
+    return results     
 
-    _, *revenue = zip(*lines[2:len(lines)//2])
-    _, *expenses = zip(*lines[2+len(lines)//2:])
-    
-    commission = [sum(max(0, int(r)-int(e))*.062 for r, e in zip(*data)) for data in zip(revenue, expenses)]
+def parse_input(lines):
+    revenues = parse_table("Revenue", lines)
+    expenses = parse_table("Expenses", lines)
+    for person in revenues:
+        comission = 0
+        for item in revenues[person]:
+            total = revenues[person][item] - expenses[person][item]
+            if total > 0:
+                comission += total * 0.062
+        print(person, format(comission, '.2f'))
 
-    print(f"           {' '.join(lines[1])}")
-    print(f"Commission {' '.join([f'{value//1:>{len(name)}.0f}' for value, name in zip(commission, lines[1])])}")
-    
-sales('''Revenue
-
-            Johnver Vanston Danbree Vansey  Mundyke
-Tea             190     140    1926     14      143
-Coffee          325      19     293   1491      162
-Water           682      14     852     56      659
-Milk            829     140     609    120       87
-
-Expenses
-
-            Johnver Vanston Danbree Vansey  Mundyke
-Tea             120      65     890     54      430
-Coffee          300      10      23    802      235
-Water            50     299    1290     12      145
-Milk             67     254      89    129       76''')
+if __name__ == '__main__':
+    with open("input.txt") as fh:
+        lines = fh.readlines()
+    clean_lines = [line.strip() for line in lines if line != "\n"]
+    parse_input(clean_lines)  

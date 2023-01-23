07c42255de3c3e2a5706d4b90a959f2991875ba7
JakobVapper <jakob.vapper@ametikool.ee>
2023-01-23 09:19:01 +0200

    updates

--- yl26.py
+++ yl26.py
@@ -5,23 +5,30 @@ def parse_table(name, lines):
     column_names = lines[index].split()
     index += 1
     rows = {}
+
     while index < len(lines) and lines[index] != stop_word:
             r = lines[index].split()
             rows[r[0]] = [int(e) for e in r[1:]]
             index+=1
+
     for col_idx, column_name in enumerate(column_names):
         results[column_name] = {}
+
         for row_index in rows:
             results[column_name][row_index] = rows[row_index][col_idx]
+
     return results     
 
 def parse_input(lines):
     revenues = parse_table("Revenue", lines)
     expenses = parse_table("Expenses", lines)
+
     for person in revenues:
         comission = 0
+
         for item in revenues[person]:
             total = revenues[person][item] - expenses[person][item]
+            
             if total > 0:
                 comission += total * 0.062
         print(person, format(comission, '.2f'))
